84112a9925ea08a03ed3c41f6dd962c9765ca1a0
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-04-10 09:49:37 +0700

    chore: move component folder to src

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,32 @@
-export * from "./components";
+export type { LinkTreeProps } from "./aside/linktree";
+export type { SelectProp } from "./dynamicsimageselect";
+
+export { default as Alert } from "./alert";
+export { default as Aside } from "./aside";
+export { default as Avatar } from "./avatar";
+export { default as AvatarPicker } from "./avatarpicker";
+export { default as Badge } from "./badge";
+export { default as Button } from "./button";
+export { default as Card } from "./card";
+export { default as Checkbox } from "./checkbox";
+export { default as Chip } from "./chip";
+export { default as Drawer } from "./drawer";
+export { default as DynamicSelect } from "./dynamicselect";
+export { default as DynamicImageSelect } from "./dynamicsimageselect";
+export { default as IconButton } from "./iconbutton";
+export { default as ImagePicker } from "./imagepicker";
+export { default as Input } from "./input";
+export { default as InputFile } from "./inputfile";
+export { default as Label } from "./label";
+export { default as LinkButton } from "./linkbutton";
+export { default as LinkOverlay } from "./linkoverlay";
+export { default as LinkBox } from "./linkoverlay/linkbox";
+export { default as ModalBackdrop } from "./modalbackdrop";
+export { default as PopUp } from "./popup";
+export { default as Select } from "./select";
+export { default as TextArea } from "./textarea";
+export { default as Toast } from "./toast";
+export { default as useToast } from "./toast/useToast";
 export {
   prepareClientPortals,
   UniversalPortal,
